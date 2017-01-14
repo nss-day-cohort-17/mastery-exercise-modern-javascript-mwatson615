49dc9c60c22a50644a86e9c0941d7aa6441f0948
Michael Watson <m.watson.tn@gmail.com>
2017-01-14 01:39:19 -0600

    create 2 models for each type

--- classes.js
+++ classes.js
@@ -8,18 +8,23 @@ function Robot(name, type, model) {
 	this.specialPower = null;
 	this.inflictDamage = null;
 	this.takeDamage = null;
-	this.health = 100;
+	this.health = null;
 
 	this.toString = function() {
 		return this.name;
 	}
 }
+function getRandomNumber(lower, upper) {
+	var randomNumber = Math.floor(Math.random() * (upper - lower + 1) + lower);
+}
+
 //define 3 robot type functions
+//each type must have a unique property aka aerial or ground-based
+
 //1. Aerial
 
 function Aerial() {
 	this.type = "aerial";
-	this.health + 20;
 }
 Aerial.prototype = new Robot();
 //2. Water
@@ -31,27 +36,61 @@ Water.prototype = new Robot();
 //3. Land
 function Land() {
 	this.type = "land";
-	this.health - 20;
 }
 
 Land.prototype = new Robot();
 //***********************************
 
-// var evilBot = new Aerial();
-// var roboSatan = new Land();
-// var fluffyBot = new Water();
-// console.log(evilBot, roboSatan, fluffyBot)
+//define at least 2 specific robot model functions for each type
+//give each robot model a unique range of health
+//give each robot model a unique range of damage
+
+//Aerial Type Models
+
+function RoboSatan() {
+	this.health = getRandomNumber(80, 120)
+	this.inflictDamage = getRandomNumber(5, 15)
+}
+RoboSatan.prototype = new Aerial();
+
+function Evilbot() {
+	this.health = getRandomNumber(70, 130);
+	this.inflictDamage = getRandomNumber(2, 20);
+}
+Evilbot.prototype = new Aerial();
+
+// *********************************
+//Land Type Models
+
+function Irritabot() {
+	this.health = getRandomNumber(90, 110);
+	this.inflictDamage = getRandomNumber(5, 20);
+}
+Irritabot.prototype = new Land();
+
+function Surlybot() {
+	this.health = getRandomNumber(75, 100);
+	this.inflictDamage = getRandomNumber(15, 20);
+}
+
+//**********************************
+//Water Type Models
+
+function Fluffy() {
+	this.health = getRandomNumber(50, 150);
+	this.inflictDamage = getRandomNumber(5, 50);
+}
+Fluffy.prototype = new Water();
+
+function Kittybot() {
+	this.health = getRandomNumber(75, 125);
+	this.inflictDamage = getRandomNumber(10, 30);
+}
+Kittybot.prototype = new Water();
+
 
 
 
 //EvilBot
 //RoboSatan
 //FluffyBot
-
-//each type must have a unique property aka aerial or ground-based
-
-//define at least 2 specific robot model functions for each type
-
-//give each robot model a unique range of health
-
-//give each robot model a unique range of damage
